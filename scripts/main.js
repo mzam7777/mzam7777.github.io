3431a295a78bc764ddaf8e77795e4bea2caaa36b
mzam777 <1zakialmubarok@gmail.com>
2024-02-24 22:40:10 +0700

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+// Toggle and Responsive Navigation
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector("nav");
+
+
+    burger.addEventListener("click", () => {
+        navLists.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+
+navSlide();
+
+
+
+
+
+
+
+
+//Clear for before unload
+
+
+
+window.onbeforeunload = () => { 
+
+    for (const form of document.getElementsByTagName("form") ){
+        form.reset()
+    }
+};
+
